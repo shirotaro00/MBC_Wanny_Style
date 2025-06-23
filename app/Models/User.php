89433dc6835aa09ec373309a0fa94099d7c10c92
kevinmarcus00 <kevinmarcusralaiaritiana@gmail.com>
2025-06-23 16:25:09 +0300

    init

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'nom',
+        'prenom',
         'email',
         'password',
+        'adresse',
+        'telephone',
+        'role'
     ];
 
     /**
