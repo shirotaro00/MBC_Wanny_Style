c45d103c93853975e1004ba061752ecf28c4b71d
shirotaro00 <rabearisonainasafidy@gmail.com>
2025-06-24 10:38:46 +0300

    Update navbar.blade.php

--- resources/views/partials/clients/navbar.blade.php
+++ resources/views/partials/clients/navbar.blade.php
@@ -33,7 +33,7 @@
             <div class="row">
                 <div class="col-xl-3 col-lg-2">
                     <div class="header__logo">
-                        <a href="./index.html"><img class="photo" src="img/logo.jpg" alt=""></a>
+                        <a href="./index.html"><img class="photo" src=" {{asset('assets/img/logo.jpg')}} s" alt=""></a>
                     </div>
                 </div>
                 <div class="col-xl-6 col-lg-7">
