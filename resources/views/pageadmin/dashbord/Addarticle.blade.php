6f2902f3d0783f347beff1160604c25ba7f650a6
shirotaro00 <rabearisonainasafidy@gmail.com>
2025-07-01 16:06:27 +0300

    Update Addarticle.blade.php

--- resources/views/pageadmin/dashbord/Addarticle.blade.php
+++ resources/views/pageadmin/dashbord/Addarticle.blade.php
@@ -34,35 +34,6 @@
 
 
 
-<!-- Modal 3 : Détails article -->
-<div class="modal fade" id="detailsModal" tabindex="-1" aria-labelledby="detailsModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <form class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="detailsModalLabel">Ajouter les détails d'article</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fermer"></button>
-      </div>
-      <div class="modal-body">
-        <div class="mb-3">
-          <label for="nomArticle" class="form-label">Nom de l'article</label>
-          <input type="text" class="form-control" id="nomArticle" name="nomArticle" required>
-        </div>
-        <div class="mb-3">
-          <label for="description" class="form-label">Description</label>
-          <textarea class="form-control" id="description" name="description" rows="3"></textarea>
-        </div>
-        <div class="mb-3">
-          <label for="prix" class="form-label">Prix</label>
-          <input type="number" class="form-control" id="prix" name="prix" required>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="submit" class="btn btn-success">Enregistrer</button>
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-      </div>
-    </form>
-  </div>
-</div>
                                 <div class="card-action">
                                     <button class="btn btn-success">Submit</button>
                                     <button class="btn btn-danger">Cancel</button>
