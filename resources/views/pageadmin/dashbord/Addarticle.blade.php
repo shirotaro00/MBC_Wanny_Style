4c0af0dc2f14fa744617e0296401391c13982a35
kevinmarcus00 <kevinmarcusralaiaritiana@gmail.com>
2025-07-01 15:22:19 +0300

    init

--- resources/views/pageadmin/dashbord/Addarticle.blade.php
+++ resources/views/pageadmin/dashbord/Addarticle.blade.php
@@ -21,6 +21,85 @@
                     <div class="card-title">Form Elements</div>
                   </div>
 
+                  <div class="my-4">
+    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#categorieModal">Catégorie article</button>
+    <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#typeModal">Type article</button>
+    <button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#detailsModal">Détails article</button>
+</div>
+
+<!-- Modal 1 : Catégorie article -->
+<div class="modal fade" id="categorieModal" tabindex="-1" aria-labelledby="categorieModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <form class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="categorieModalLabel">Ajouter une catégorie</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fermer"></button>
+      </div>
+      <div class="modal-body">
+        <div class="mb-3">
+          <label for="nomCategorie" class="form-label">Nom de la catégorie</label>
+          <input type="text" class="form-control" id="nomCategorie" name="nomCategorie" required>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-success">Enregistrer</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+      </div>
+    </form>
+  </div>
+</div>
+
+<!-- Modal 2 : Type article -->
+<div class="modal fade" id="typeModal" tabindex="-1" aria-labelledby="typeModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <form class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="typeModalLabel">Ajouter un type d'article</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fermer"></button>
+      </div>
+      <div class="modal-body">
+        <div class="mb-3">
+          <label for="nomType" class="form-label">Nom du type</label>
+          <input type="text" class="form-control" id="nomType" name="nomType" required>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-success">Enregistrer</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+      </div>
+    </form>
+  </div>
+</div>
+
+<!-- Modal 3 : Détails article -->
+<div class="modal fade" id="detailsModal" tabindex="-1" aria-labelledby="detailsModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <form class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="detailsModalLabel">Ajouter les détails d'article</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fermer"></button>
+      </div>
+      <div class="modal-body">
+        <div class="mb-3">
+          <label for="nomArticle" class="form-label">Nom de l'article</label>
+          <input type="text" class="form-control" id="nomArticle" name="nomArticle" required>
+        </div>
+        <div class="mb-3">
+          <label for="description" class="form-label">Description</label>
+          <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="prix" class="form-label">Prix</label>
+          <input type="number" class="form-control" id="prix" name="prix" required>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-success">Enregistrer</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+      </div>
+    </form>
+  </div>
+</div>
                   <div class="card-action">
                     <button class="btn btn-success">Submit</button>
                     <button class="btn btn-danger">Cancel</button>
