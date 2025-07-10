e05b7fe259ba64bf7d782615a76fa17fb21e8bb2
shirotaro00 <rabearisonainasafidy@gmail.com>
2025-07-10 16:16:02 +0300

    init

--- resources/views/partials/clients/modal/inscription.blade.php
+++ resources/views/partials/clients/modal/inscription.blade.php
@@ -15,41 +15,85 @@
                 <form action="{{ route('client.register') }}" method="POST">
                     @csrf
                     <div class="row">
+                        <div class="mb-3">
+                            <label for="nom" class="form-label">Nom</label>
+                            <input type="text" class="form-control @error('nom') is-invalid @enderror" id="nom"
+                                name="nom">
+                            @error('nom')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
                         <div class="col-md-6">
-                            <div class="mb-3">
-                                <label for="nom" class="form-label">Nom</label>
-                                <input type="text" class="form-control" id="nom" name="nom">
-                            </div>
+
                             <div class="mb-3">
                                 <label for="prenom" class="form-label">Prénom</label>
-                                <input type="text" class="form-control" id="prenom" name="prenom">
+                                <input type="text" class="form-control @error('prenom') is-invalid @enderror"
+                                    id="prenom" name="prenom">
+                                @error('prenom')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
+
                             <div class="mb-3">
                                 <label for="adresse" class="form-label">Adresse</label>
-                                <input type="text" class="form-control" id="adresse" name="adresse">
+                                <input type="text" class="form-control @error('adresse') is-invalid @enderror"
+                                    id="adresse" name="adresse">
+                                @error('adresse')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="telephone" class="form-label">Telephone</label>
+                                <input type="text" class="form-control @error('telephone') is-invalid @enderror"
+                                    id="telephone" name="telephone">
+                                @error('telephone')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <div class="mb-3">
-                                <label for="telephone" class="form-label">Telephone</label>
-                                <input type="text" class="form-control" id="telephone" name="telephone">
-                            </div>
+
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="email" name="email">
+                                <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                    id="email" name="email">
+                                @error('email')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="telephone" class="form-label">Mot de passe</label>
-                                <input type="password" class="form-control" id="password" name="password">
+                                <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                    id="password" name="password">
+                                @error('password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
-                                <label for="telephone" class="form-label">Confirmation mots de passe</label>
-                                <input type="password" name="password_confirmation" class="form-control" id="password"
-                                    required />
+                                <label for="telephone" class="form-label">Confirme mots de passe</label>
+                                <input type="password" name="password_confirmation"
+                                    class="form-control @error('password_confirmation') is-invalid @enderror"
+                                    id="password_confirmation" required />
+                                @error('password_confirmation')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
-                             @error('password') <small class="text-danger" style="color: red">
-                                {{ $message }}</small>
-                            @enderror
+
                         </div>
                     </div>
 
