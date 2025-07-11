1d8a9cab7787b40762a5e68356b85be87ec9803b
kevinmarcus00 <kevinmarcusralaiaritiana@gmail.com>
2025-07-11 13:46:09 +0300

    init

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[ClientController::class, 'accueil'])->name('page.accueil');
-
+Route::get('/article',[ClientController::class, 'article'])->name('page.article');
 
 // route pour admin
 Route::get('/connexion',[AdminController::class, 'admin'])->name('page.admin');
